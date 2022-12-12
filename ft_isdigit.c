1d5ed578e918f9dea9683eb851dcb0a0ddf15520
dbizhano <bizdenis3@gmail.com>
2022-12-12 19:43:47 +0000

    finally set it up

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbizjano <dbizjano@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 19:41:50 by dbizjano          #+#    #+#             */
+/*   Updated: 2022/12/12 19:41:51 by dbizjano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int ft_isdigit(int ch)
 {
     if (ch >= '0' && ch <= '9')
@@ -7,3 +19,12 @@ int ft_isdigit(int ch)
     else
         return 0;
 }
+
+// int main(void)
+// {
+//     char    c = '5';
+//     int     res = ft_isdigit(c);
+
+//     printf("res = %d", res);
+//     return 0;
+// }
