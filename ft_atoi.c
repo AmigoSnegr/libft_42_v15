0aa65edce54fd1e3b3d5cabe89b6c762374e4b61
dbizhano <bizdenis3@gmail.com>
2022-12-13 20:33:29 +0000

    atoi, toupper, tolower done

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dbizjano <dbizjano@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 11:43:49 by dbizjano          #+#    #+#             */
-/*   Updated: 2022/12/13 17:32:17 by dbizjano         ###   ########.fr       */
+/*   Updated: 2022/12/13 20:22:42 by dbizjano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,43 @@
 #include <stdio.h>
 #include "libft.h"
 
-int	algorythm(const char *str, size_t i, int minus, int result)
+static int	algorythm(const char *str, size_t i, int result, int minus)
 {
+	int	current;
 	int	next;
 
 	while (str[i] != 0)
 	{
-		if (i != (ft_strlen(str) - 1))
-			next = ft_isdigit(str[i + 1]);
-		if (str[i] >= 48 && str[i] <= 57)
+		current = ft_isdigit(str[i]);
+		next = ft_isdigit(str[i + 1]);
+		if ((current == 0 && next == 0)
+			|| (current == 0 && next != 0 && str[i] != '-'))
+			return (0);
+		if (str[0] == '-' && next != 0)
+			minus = -1;
+		if (current != 0)
 		{
-			if (str[0] == '-' && !(str[1] > 47 && str[1] < 58))
-				return (0);
-			if (str[i - 1] == '-')
-				minus = 1;
 			result = result * 10 + str[i] - 48;
-			if (result == 214748364 && minus == 1 && str[i + 1] == '8')
-				return (-2147483648);
-			if (next == 0)
+			if (current != 0 && next == 0)
 				break ;
+			if (result == 214748364 && str[i + 1] == '8' && minus == 1)
+			{
+				return (-2147483648);
+			}
 		}
 		i++;
 	}
-	if (minus == 1)
-		return (-result);
-	return (result);
+	return (result * minus);
 }
 
 int	ft_atoi(const char *str)
 {
 	int		result;
 	int		minus;
-	int		k;
 	size_t	i;
 
 	i = 0;
-	minus = 0;
+	minus = 1;
 	result = 0;
-	k = algorythm(str, i, minus, result);
-	return (k);
+	return (algorythm(str, i, result, minus));
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dbizjano <dbizjano@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 19:42:08 by dbizjano          #+#    #+#             */
-/*   Updated: 2022/12/13 17:32:43 by dbizjano         ###   ########.fr       */
+/*   Updated: 2022/12/13 20:23:54 by dbizjano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	main(void)
 {
-	char	c[] = "2147483648ij44";
+	char	c[] = "-99t9";
 	printf("res = %d\n", ft_atoi(c));
 	printf("res = %d\n", atoi(c));
 	return (0);
