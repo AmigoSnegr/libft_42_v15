175581838f6b1cb748c7b4707ac8306a0f7e7aa4
dbizhano <bizdenis3@gmail.com>
2023-01-17 17:27:54 +0000

    ft_split been done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dbizjano <dbizjano@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 19:41:59 by dbizjano          #+#    #+#             */
-/*   Updated: 2023/01/07 23:44:01 by dbizjano         ###   ########.fr       */
+/*   Updated: 2023/01/16 22:28:56 by dbizjano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	**ft_split(char const *s, char c);
 
 #endif
