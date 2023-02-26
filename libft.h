383a18e8eb50d2f5fdc0f1fbf4852e7d5b8c6963
dbizhano <bizdenis3@gmail.com>
2023-02-26 19:43:37 +0000

    fixed make

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: debizhan <debizhan@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 19:41:59 by dbizjano          #+#    #+#             */
-/*   Updated: 2023/02/23 17:50:51 by debizhan         ###   ########.fr       */
+/*   Updated: 2023/02/23 18:09:14 by debizhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
-void	ft_lstadd_back(t_list **lst, t_list *(new));
+void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
